a9039042af1dea1f88f4d5f2fee6a6f987244ff3
Marcia Alves <marciasoalves@gmail.cm>
2026-04-08 22:10:17 -0300

    Deepening in collections

--- fundamentos-java/cardapio/src/main/java/mx/florinda/cardapio/CardapioApplication.java
+++ fundamentos-java/cardapio/src/main/java/mx/florinda/cardapio/CardapioApplication.java
@@ -1,26 +1,31 @@
 package mx.florinda.cardapio;
 
-import com.google.gson.Gson;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
 
-import static mx.florinda.cardapio.ItemCardapio.CategoriaCardapio.BEBIDAS;
 
 @SpringBootApplication
 public class CardapioApplication {
 
 	public static void main(String[] args) {
-		ItemCardapio refrescoDoChaves = new ItemCardapio(1L, "Refresco do Chaves", """
-                Suco de limão que parece tamarindo e tem gosto de groselha
-                """, BEBIDAS,
-				new BigDecimal("2.99"), null);
+		Database database = new Database();
 
-		Gson gson = new Gson();
-		String json = gson.toJson(refrescoDoChaves);
+		ItemCardapio item = database.itemCardapioPorId(1L).orElseThrow(); // 2.99
 
-		System.out.println(json);
+		database.alteraPrecoItemCardapio(1L, new BigDecimal("3.99")); // 2.99 => 3.99
+		ItemCardapio item1 = database.itemCardapioPorId(1L).orElseThrow(); // 3.99
+
+		database.alteraPrecoItemCardapio(1L, new BigDecimal("2.99")); // 3.99 => 2.99
+		ItemCardapio item2 = database.itemCardapioPorId(1L).orElseThrow(); // 2.99
+
+		database.alteraPrecoItemCardapio(1L, new BigDecimal("4.99")); // 2.99 => 4.99
+		ItemCardapio item3 = database.itemCardapioPorId(1L).orElseThrow(); // 4.99
+
+		System.out.println("== " + (item == item2));
+		System.out.println("equals() " + (item.equals(item2)));
+		System.out.println("hashCode() " + (item.hashCode() == item2.hashCode()));
+
+		database.rastroAuditoriaPrecos();
 	}
-
 }
